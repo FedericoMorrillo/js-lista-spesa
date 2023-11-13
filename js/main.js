5a94e110f7a7112d96af5ab7d3d77b15a9d50574
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-13 17:53:39 +0100

    feat: lista completata

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 'use strict'
 
+// associamo l' ul alla variabile
+const lista = document.querySelector('.list');
+console.log(lista);
+
+// array di prodotti
+const spesa = ['Pane', 'Insalata', 'Uova', 'Pasta','Riso','Cozze','Patate','Melograno','Kiwi', 'Farina', 'Vino rosso',];
+console.log(spesa);
+
+//dichiarazione
+let i = 0;
+
+//ciclo
+//****************************************
+ while(i < spesa.length - 1){
+
+     //creiamo il li
+     let prodotto = document.createElement("li");
+
+     //assegniamo la classe
+     prodotto.classList.add('prodotto');
+
+     //inseriamo il prodotto dell' indice
+     prodotto.textContent = spesa[i];
+
+ // appendiamo il li nell' ul
+     lista.append(prodotto);
+
+    //incremento
+      i++;
+
+    // aggiungiamo un evento on click
+    prodotto.addEventListener('click', function(event) {
+    prodotto.classList.toggle('comprato');    
+    console.log("ho cliccato");
+    console.log(event);
+    }) 
+ }
+//*****************************************
+
+
 
